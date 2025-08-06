882f896f1140b8f08b894cec42f4e4520822b863
filipyuen <158810423+filipyuen@users.noreply.github.com>
2025-08-06 22:53:29 +0100

    Delete test_keyboard.py

--- test_keyboard.py
+++
@@ -1,7 +0,0 @@
-import keyboard
-
-def print_key_event(event):
-    print(f"Event: name={event.name}, scan_code={event.scan_code}, is_keypad={event.is_keypad}, event_type={event.event_type}")
-
-keyboard.hook(print_key_event)
-keyboard.wait('esc')  # 按 Esc 退出
